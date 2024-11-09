d2208829aa2dd924d1bf67148d6454898b77f4f2
2blackcow <138091561+2blackcow@users.noreply.github.com>
2024-11-09 23:28:39 +0900

    페이지 구현 및 수정

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Deploy Test!!!!!"/>
+  <div id="app">
+    <!-- 네비게이션 바 -->
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/">홈</router-link></li>
+          <li><router-link to="/popular">대세 콘텐츠</router-link></li>
+          <li><router-link to="/search">찾아보기</router-link></li>
+          <li><router-link to="/wishlist">내가 찜한 리스트</router-link></li>
+        </ul>
+        <div class="user-icon">
+          <router-link to="/signin">
+            <i class="fas fa-user"></i>
+          </router-link>
+        </div>
+      </nav>
+    </header>
+
+    <!-- 메인 콘텐츠 -->
+    <main>
+      <router-view />
+    </main>
+
+    <!-- 푸터 (필요 시 추가) -->
+    <footer>
+      <p>&copy; 2024 Movie Demo</p>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
+export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+};
 </script>
 
-<style>
+<style scoped>
+/* 기본 레이아웃 스타일 */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, sans-serif;
+  color: #fff;
+  background-color: #181818;
+}
+
+header {
+  background-color: #333;
+  padding: 10px;
+}
+
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline;
+  margin-right: 20px;
+}
+
+nav ul li a {
+  color: white;
+  text-decoration: none;
+}
+
+.user-icon {
+  float: right;
+}
+
+footer {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // 라우터 설정을 가져옴
+import './assets/styles.css'; // 전역 스타일
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router) // 라우터 사용
+    .mount('#app');
